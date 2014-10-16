dfdcf50314581d85982ae8c3dd8850ced763b4b1
Diego Herranz <diegoherranz@diegoherranz.com>
2014-10-16 21:44:50 +0200

    Add seven-segment display examples

---
+++ pic14/2.seven_seg/README.md
@@ -0,0 +1,11 @@
+7-Segment display
+=================
+
+Count on a 7 segment display approximately at a 1 Hz frequency.
+
+### Hardware for this example
+ - Reset input pin must be connected to VDD.
+ - A common cathode 7 segment display must be connected to PORTB. Common cathode connected to GND and anodes connected to the PIC pin through a ~1k ohm resistor each. Display segment A should be connected to RB0, segment B to RB1 and so on until segment G connected to RB6. [Seven-segment display](http://en.wikipedia.org/wiki/Seven-segment_display) on Wikipedia.
+
+### Notes
+ - Function `number_to_7_seg` converts from an integer number to which segments must be on/off.
---
+++ pic14/2.seven_seg/seven_seg.c
@@ -0,0 +1,61 @@
+#define NO_BIT_DEFINES
+#include <pic14regs.h>
+#include <stdint.h> 
+
+// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+__code uint16_t __at (_CONFIG1) __configword = _INTRC_OSC_NOCLKOUT;
+
+
+// Uncalibrated delay, just waits a number of for-loop iterations
+void delay(uint16_t iterations){
+		uint16_t i;
+		for (i = 0; i < iterations; i++) {
+			// Prevent this loop from being optimized away.
+			__asm nop __endasm;
+		}
+}
+
+uint8_t number_to_7seg(uint8_t number){
+
+	switch(number){
+		case 0:
+			return 0x3F;
+		case 1:
+			return 0x06;
+		case 2:
+			return 0x5B;
+		case 3:
+			return 0x4F;
+		case 4:
+			return 0x66;
+		case 5:
+			return 0x6D;
+		case 6:
+			return 0x7D;
+		case 7:
+			return 0x07;
+		case 8:
+			return 0x7F;
+		case 9:
+			return 0x6F;
+		default:
+			return 0;
+	}
+
+}
+
+void main(void){
+
+	uint8_t i;
+
+	TRISB = 0; // Pins as output
+	PORTB = 0; // LEDs off
+	
+	while(1){
+		for(i=0; i<10; i++){
+			PORTB = number_to_7seg(i);
+			delay(60000); //~1s @ 4MHz	
+		}		
+	}
+
+}
